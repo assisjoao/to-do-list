274b07e0a2c14599846a3c5d9724535b7ec2be33
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-16 08:40:54 -0300

    Making the item done/undone

--- src/List.js
+++ src/List.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 
+function DoneImg(props){
 
+        if(props.done){
+                return (<i alt="done" class="fas fa-check-square"></i>)
+        } else{
+                return (<i alt="undone" class="far fa-check-square"></i>)
+        }
+}
 
 
 function List(props){
@@ -9,8 +16,9 @@ function List(props){
     
 
     return(<ul>
-            {props.itens.map(item=><li key={item.id}>
+            {props.itens.map(item=><li className={item.done ? "done" : ""} key={item.id}>
                 {item.text}
+                <button onClick={()=>{props.onDone(item)}}><DoneImg done={item.done}></DoneImg></button>
                 <button onClick={()=>{props.onItemDeleted(item)}}><img alt="delete" src="./assets/bin.png"></img></button>
                 </li>)}
         </ul>)
--- src/Todo.js
+++ src/Todo.js
@@ -25,12 +25,25 @@ function Todo(){
 
     }
 
+    function onDone(item){
+
+        let updatedItens = itens.map(it=>{
+            if(it.id === item.id){
+                it.done = !it.done;
+            }
+            return it;
+        })
+
+        setItens(updatedItens);
+
+    }
+
     return (<div className='container'>
                 <h1>Todo</h1>
                 <TodoForm onAddItem={onAddItem}></TodoForm>
                 
 
-                <List onItemDeleted={onItemDeleted} itens={itens}></List>
+                <List onDone={onDone} onItemDeleted={onItemDeleted} itens={itens}></List>
             </div>);
 
 }
